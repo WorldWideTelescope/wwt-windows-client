bd1db0c74ae092e7d28618f3f43ef51cbfd80238
Jonathan Fay <jfay@fayhome.com>
2017-08-08 10:42:48 -0700

    Added Support for SCISS SGC Warp/Blend

--- WWTExplorer3d/Properties/AssemblyInfo.cs
+++ WWTExplorer3d/Properties/AssemblyInfo.cs
@@ -30,8 +30,8 @@ using System.Resources;
 //      Build Number
 //      Revision
 //
-[assembly: AssemblyVersion("5.5.22.1")]
-[assembly: AssemblyFileVersion("5.5.22.1")]
+[assembly: AssemblyVersion("5.5.24.1")]
+[assembly: AssemblyFileVersion("5.5.24.1")]
 [assembly: NeutralResourcesLanguageAttribute("en-US")]
 [assembly: System.Windows.Media.DisableDpiAwareness]
 
