96352db69ba92d4a42be40541d4a7fa013bb7f3d
Peter Williams <peter@newton.cx>
2022-01-05 11:10:44 +0000

    Setup1/CustomAction_NoImpersonate.js: reformat a la VS Code

--- Setup1/CustomAction_NoImpersonate.js
+++ Setup1/CustomAction_NoImpersonate.js
@@ -4,19 +4,18 @@
 // Constant values from Windows Installer
 var msiOpenDatabaseModeTransact = 1;
 
-var msiViewModifyInsert         = 1
-var msiViewModifyUpdate         = 2
-var msiViewModifyAssign         = 3
-var msiViewModifyReplace        = 4
-var msiViewModifyDelete         = 6
+var msiViewModifyInsert = 1
+var msiViewModifyUpdate = 2
+var msiViewModifyAssign = 3
+var msiViewModifyReplace = 4
+var msiViewModifyDelete = 6
 
-var msidbCustomActionTypeInScript       = 0x00000400;
-var msidbCustomActionTypeNoImpersonate  = 0x00000800
+var msidbCustomActionTypeInScript = 0x00000400;
+var msidbCustomActionTypeNoImpersonate = 0x00000800
 
-if (WScript.Arguments.Length != 1)
-{
-	WScript.StdErr.WriteLine(WScript.ScriptName + " file");
-	WScript.Quit(1);
+if (WScript.Arguments.Length != 1) {
+    WScript.StdErr.WriteLine(WScript.ScriptName + " file");
+    WScript.Quit(1);
 }
 
 var filespec = WScript.Arguments(0);
@@ -27,27 +26,23 @@ var sql
 var view
 var record
 
-try
-{
-	sql = "SELECT `Action`, `Type`, `Source`, `Target` FROM `CustomAction`";
-	view = database.OpenView(sql);
-	view.Execute();
-	record = view.Fetch();
-	while (record)
-	{
-	    if (record.IntegerData(2) & msidbCustomActionTypeInScript)
-	    {
-	        record.IntegerData(2) = record.IntegerData(2) | msidbCustomActionTypeNoImpersonate;
-        	view.Modify(msiViewModifyReplace, record);
-	    }
+try {
+    sql = "SELECT `Action`, `Type`, `Source`, `Target` FROM `CustomAction`";
+    view = database.OpenView(sql);
+    view.Execute();
+    record = view.Fetch();
+    while (record) {
+        if (record.IntegerData(2) & msidbCustomActionTypeInScript) {
+            record.IntegerData(2) = record.IntegerData(2) | msidbCustomActionTypeNoImpersonate;
+            view.Modify(msiViewModifyReplace, record);
+        }
         record = view.Fetch();
-	}
+    }
 
-	view.Close();
-	database.Commit();
+    view.Close();
+    database.Commit();
 }
-catch(e)
-{
-	WScript.StdErr.WriteLine(e);
-	WScript.Quit(1);
+catch (e) {
+    WScript.StdErr.WriteLine(e);
+    WScript.Quit(1);
 }
